be7b9e29a1b3a59907fd7ce8cba2f51b7c1c7771
Pastisseria <pcusachs@gmail.com>
2026-09-24 19:47:08 +0200

    Cargar estilos específicos de móvil

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ import './finalizarFacturacionPresupuesto.js'
 import './datosFacturaSimplificado.js'
 import './historicoFacturacionFiltro.js'
 import './produccionPrintA4.css'
+import './mobile.css'
 import App from './App.jsx'
 import { AuthProvider } from './auth/AuthContext.jsx'
 
