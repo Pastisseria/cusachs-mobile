9f70aea7038554eda97e7dfdf36436697f69d974
Pastisseria <pcusachs@gmail.com>
2026-09-24 20:23:51 +0200

    Añadir configuración Android de Cusachs Mobile

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cusachs.mobile',
+  appName: 'Cusachs',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+};
+
+export default config;
